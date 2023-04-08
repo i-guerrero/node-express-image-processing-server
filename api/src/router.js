e7a28692a1027ff5fea566a17836ad7135875f6c
i-guerrero <isaacguerrero@pursuit.org>
2023-04-08 19:41:40 -0400

    Invoke the image processor in the post route

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
+const imageProcessor = require("./imageProcessor");
 
 const router = Router();
 
@@ -29,12 +30,16 @@ function fileFilter(request, file, callback) {
   }
 }
 
-router.post("/upload", upload.single("photo"), (request, response) => {
+router.post("/upload", upload.single("photo"), async (request, response) => {
   if (request.fileValidationError) {
     response.status(400).json({ error: request.fileValidationError });
   } else {
     response.status(201).json({ success: true });
   }
+
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {}
 });
 
 router.get("/photo-viewer", (request, response) => {
